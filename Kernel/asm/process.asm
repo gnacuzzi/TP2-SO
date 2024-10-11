b0f0aacdaf6ced3ef6dc44c933e046e6236c6d07
Gianna Nacuzzi <giannanacuzzi@MBP-de-Gianna.fibertel.com.ar>
2024-10-11 10:50:43 -0300

    reordenamos nuestra forma de pasar la pila y ahora los programas reciben correctamente argv y argc

--- Kernel/asm/process.asm
+++ Kernel/asm/process.asm
@@ -14,23 +14,23 @@ setupStackFrame:
     push 0x8
     push rsi
 
-    push r15
-	push r14
-	push r13
-	push r12
+    push rax
+    push rbx
+    push rdi
+    push rsi
+    push rbp
+
+    push rdx
+    push rcx
+    
+    push r8
+    push r9
+    push r10
 	push r11
-	push r10
-	push r9
-	push r8
-
-	push rcx 
-	push rdx 
-
-	push rbp
-	push rsi 
-	push rdi 
-	push rbx
-	push rax
+	push r12
+	push r13
+	push r14
+	push r15
 
     mov rax, rsp
     mov rsp, rbp
