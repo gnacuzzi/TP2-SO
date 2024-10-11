c5b5ca845bf63fba2b5c817581b8e26f5a42f7b1
Gianna Nacuzzi <giannanacuzzi@MBP-de-Gianna.fibertel.com.ar>
2024-10-11 08:52:28 -0300

    modificamos scheduler.h porque un cambio habia generado un error

--- Kernel/include/scheduler.h
+++ Kernel/include/scheduler.h
@@ -12,14 +12,14 @@ void createScheduler();
 
 uint64_t schedule(uint64_t prevRSP);
 
-uint16_t createProcess(uint64_t rip, char **args, int argc,char *name, uint8_t priority, int16_t fileDescriptors[]);
-int64_t blockProcess(uint16_t pid);
-int64_t readyProcess(uint16_t pid);
-PCB *findProcess(uint16_t pid);
+int16_t createProcess(uint64_t rip, char **args, int argc,char *name, uint8_t priority, int16_t fileDescriptors[]);
+int64_t blockProcess(int16_t pid);
+int64_t readyProcess(int16_t pid);
+PCB *findProcess(int16_t pid);
 void yield();
 int64_t killCurrentProcess();
-int64_t killProcess(uint16_t pid);
-uint16_t getPid();
+int64_t killProcess(int16_t pid);
+int16_t getPid();
 //para poder hacer la de imprimir los procesos necesito la funcion de process que imprima?
 
 #endif
