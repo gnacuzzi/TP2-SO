186654577db33c0244b7f64defaf2108fcd7dc08
Pilar Frutos <pilarfrutos@PilarsMcBookAir.fibertel.com.ar>
2024-10-14 10:13:58 -0300

    .

--- Userland/SampleCodeModule/processes.c
+++ Userland/SampleCodeModule/processes.c
@@ -2,39 +2,42 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "include/processes.h"
 #include "include/tests.h"
+#include "include/syscall.h"
 #include <stdio.h>
 
 void testMemory(char parameters[MAX_PARAMETERS][PARAMETERS_LENGTH], int cantParams){
     
     if(cantParams != 2){
         printf("You must insert ONE parameter indicating the number of processes you desire to test\n");
-        return;
+        sysexit();
     }
 
     char *params[] = { parameters[1] };
     int out = test_mm(1, params);
 
     printf("Memory test %s\n", out == 0 ? "passed" : "failed");
+    sysexit();
 
 }
 
 void testPrio(char parameters[MAX_PARAMETERS][PARAMETERS_LENGTH], int cantParams){
     if(cantParams != 1){
         printf("TestPrio doesn't need parameters\n");
-        return;
+        sysexit();
     }
     test_prio();
+    sysexit();
 }
 
 void testProcesses(char parameters[MAX_PARAMETERS][PARAMETERS_LENGTH], int cantParams){
     if (cantParams != 2) {
         printf("You must insert ONE parameter indicating the number of processes you desire to test\n");
-        return;
+        sysexit();;
     }
 
     if (satoi(parameters[1]) <= 0) {
         printf("Number of processes must be greater than 0\n");
-        return;
+        sysexit();;
     }
 
     char *params[] = {parameters[1]};
@@ -42,4 +45,5 @@ void testProcesses(char parameters[MAX_PARAMETERS][PARAMETERS_LENGTH], int cantP
     int out = test_processes(1, params);
 
     printf("Process test %s\n", out == 0 ? "passed" : "failed");
+    sysexit();
 }
