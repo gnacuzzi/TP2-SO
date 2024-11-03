5b6ec7cc17a570deda8c862d785585e50f2de39a
Pilar Frutos <pilarfrutos@PilarsMcBookAir.fibertel.com.ar>
2024-11-03 17:20:55 -0300

    intentamos de meter pipes en la shell

--- Userland/SampleCodeModule/shell.c
+++ Userland/SampleCodeModule/shell.c
@@ -14,6 +14,9 @@
 #define MAX_PARAMETERS 2 // todavia no sabemos cuantos parametros se van a enviar como maximo
 #define PARAMETERS_LENGTH 256
 
+#define READ 0
+#define WRITE 1
+
 typedef void (*functionPointer)(int argc, char *argv[]);
 
 typedef struct command {
@@ -247,6 +250,63 @@ int commandId(char *command) {
     return -1;
 }
 
+static int parseCommands(char *buffer, char *leftCommand, char *leftParams[], int *leftCantParams, char *rightCommand, char *rightParams[], int *rightCantParams) {
+    char *pipe = strchr(buffer, '/');
+    if (pipe == NULL) {
+        return 0; 
+    }
+    *pipe = '\0';
+    *leftCantParams = scanCommand(leftCommand, leftParams, buffer);
+    *rightCantParams = scanCommand(rightCommand, rightParams, pipe + 2);
+
+    return 1; 
+}
+
+void executePipedCommands(char *leftCommand, char *leftParams[], int leftCantParams, int leftId, int isBackground1, char *rightCommand, char *rightParams[], int rightCantParams, int rightId, int isBackground2) {
+    int readPipe = sysgetpid();
+    if (sysopenPipe(readPipe, READ) == -1) {
+        printf("Error creating pipe read\n");
+        return;
+    }
+    int16_t pid1 = syscreateProcess((uint64_t)processCommands[leftId].exec, (char **)leftParams, leftCantParams, 1, (int16_t[]){readPipe, STDOUT, STDERR}, isBackground1);
+    if (pid1 == -1) {
+        printf("Error creating process for command: %s\n", leftCommand);
+        return;
+    }
+
+	int writePipe = sysgetpid();
+	if (sysopenPipe(writePipe, WRITE) == -1) {
+        printf("Error creating pipe write\n");
+        return;
+    }
+    int16_t pid2 = syscreateProcess((uint64_t)processCommands[rightId].exec, (char **)rightParams, rightCantParams, 1, (int16_t[]){STDIN, writePipe, STDERR}, isBackground2);
+    if (pid2 == -1) {
+        printf("Error creating process for command: %s\n", rightCommand);
+		syskillProcess(pid1);
+        return;
+    }
+
+    if (sysclosePipe(readPipe) == -1) {
+        printf("Error closing pipe read\n");
+        syskillProcess(pid1);
+        syskillProcess(pid2);
+        return;
+    }
+    if (sysclosePipe(writePipe) == -1) {
+        printf("Error closing pipe write\n");
+        syskillProcess(pid1);
+        syskillProcess(pid2);
+        return;
+    }
+
+    if (!isBackground1) {
+        syswaitProcess(pid1);
+    }
+    if (!isBackground2) {
+        syswaitProcess(pid2);
+    }
+}
+
 int main() {
 	printf("Welcome to our shell! Write which module you want to use. To see our modules write help\n");
 	printf("~$");
@@ -255,39 +315,74 @@ int main() {
 	while (1) {
 		int rta = scanf(buffer);
 		if (rta == 1) {
-			char command[BUFFER_LENGTH] = {0};
-			char *params[PARAMETERS_LENGTH] = {0};
-			int cantParams = scanCommand(command, params, buffer);
-			int id = commandId(command);
-			if (id >= 0 && id < builtinsDim) {
-				builtInCommands[id].exec(cantParams, params);
-			} else if (id >= builtinsDim && id < builtinsDim + processDim) {
-				int processIndex = id - builtinsDim;	
-				uint64_t rip = (uint64_t)processCommands[processIndex].exec;
+			char leftCommand[BUFFER_LENGTH] = {0};
+			char *leftParams[PARAMETERS_LENGTH] = {0};
+			char rightCommand[BUFFER_LENGTH] = {0};
+			char *rightParams[PARAMETERS_LENGTH] = {0};
+			int leftCantParams;
+			int rightCantParams;
+			
+			if(parseCommands(buffer, leftCommand, leftParams, &leftCantParams, rightCommand, rightParams, &rightCantParams)){
+				int leftId = commandId(leftCommand) - builtinsDim;
+				int rightId = commandId(rightCommand) - builtinsDim;
+				if(strcmp(leftCommand, processCommands[leftId].name) == 0 && strcmp(rightCommand, processCommands[rightId].name) == 0){
+					char *newParams1[MAX_PARAMETERS + 1] = {0};
+					newParams1[0] = processCommands[leftId].name;
+            		for (int i = 0; i < leftCantParams; i++) {
+               			newParams1[i + 1] = leftParams[i];
+            		}
+					leftCantParams++;
+					int isBackground1 = strcmp(leftParams[leftCantParams-1], "BACK") == 0;
 
-				char *newParams[MAX_PARAMETERS + 1] = {0};
-				newParams[0] = processCommands[processIndex].name;
-            	for (int i = 0; i < cantParams; i++) {
-               		newParams[i + 1] = params[i];
-            	}
-				int16_t fileDescriptors[] = {0, 1, 2};	
-				int isBackground = strcmp(params[cantParams-1], "BACK") == 0;
-				int16_t pid = syscreateProcess(rip, (char **)newParams, cantParams + 1, 1, fileDescriptors, isBackground);
-				if(pid == -1){
-					printf("Error creating process\n");
+					char *newParams2[MAX_PARAMETERS + 1] = {0};
+					newParams2[0] = processCommands[rightId].name;
+            		for (int i = 0; i < rightCantParams; i++) {
+               			newParams2[i + 1] = rightParams[i];
+            		}
+					rightCantParams++;
+					int isBackground2 = strcmp(rightParams[rightCantParams-1], "BACK") == 0;
+
+					executePipedCommands(leftCommand, newParams1, leftCantParams,leftId, isBackground1, rightCommand, newParams2, rightCantParams,rightId, isBackground2);
+				} else {
+					printf("Pipes only support processes\n");
 				}
-				if (!isBackground) {
-					syswaitProcess(pid);
-				}
+
 			} else {
-				printf("%s",command);
-				printf(": command not found\n");
+				leftCantParams = scanCommand(leftCommand, leftParams, buffer);
+				int id = commandId(leftCommand);
+
+				if (id >= 0 && id < builtinsDim) {
+					builtInCommands[id].exec(leftCantParams, leftParams);
+				} else if (id >= builtinsDim && id < builtinsDim + processDim) {
+					int processIndex = id - builtinsDim;	
+					uint64_t rip = (uint64_t)processCommands[processIndex].exec;
+
+					char *newParams[MAX_PARAMETERS + 1] = {0};
+					newParams[0] = processCommands[processIndex].name;
+            		for (int i = 0; i < leftCantParams; i++) {
+               			newParams[i + 1] = leftParams[i];
+            		}
+					int16_t fileDescriptors[] = {0, 1, 2};	
+					int isBackground = strcmp(leftParams[leftCantParams-1], "BACK") == 0;
+					int16_t pid = syscreateProcess(rip, (char **)newParams, leftCantParams + 1, 1, fileDescriptors, isBackground);
+					if(pid == -1){
+						printf("Error creating process\n");
+					}
+					if (!isBackground) {
+						syswaitProcess(pid);
+					}
+				} else {
+					printf("%s", leftCommand);
+					printf(": command not found\n");
+				}
 			}
+			
 			for (int i = 0; buffer[i] != 0; i++) { // vaciamos el buffer
 				buffer[i] = '\0';
 				// También vaciamos los parámetros para evitar residuos
 				for (int j = 0; j < PARAMETERS_LENGTH; j++) {
-					params[j] = NULL;
+					leftParams[j] = NULL;
+					rightParams[j] = NULL;
 				}
 			}
 		}
